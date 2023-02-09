2e369c1d1f3d57692595e5e94706e5b4f15be611
samuelberhane <1samibrhane@gmail.com>
2023-02-09 18:01:24 +0300

    Remove permission file

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,16 +1,14 @@
 import { buffer } from "micro";
 let admin = require("firebase-admin");
-// let serviceAccount = require("../../firebase/permission.json");
 const stripe = require("stripe")(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
 const endpointSecret = process.env.NEXT_PUBLIC_STRIPE_SIGNIN_SECRET;
-import app from "../../firebase/config";
 
 const app = !admin.apps.length
   ? admin.initializeApp({
       credential: admin.credential.cert({
         type: "service_account",
         project_id: "clone-ff05e",
-        private_key_id: "333f8dd692eb2daca56403207492862e0860f185",
+        private_key_id: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
         private_key:
           "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCp6Hj66DGubejr\nhGQLGTCuPuC2IhUILbjARxKfLLrPBudSJBinhDapVUHQh/pu+eSOZYzy8xSQsDHv\nT3Yx5U8QGPwcspofyj3G9OzVp9DjUV79rsGYocGzRaKv/CJlw2s7b3qgddDBiM1h\ngoEtlJDBabQF1V8BEMtsMb7P4wvyPhvUE3j5v23UVKChQV+svH91YxTjIbjnA6ne\nlysFrhVCkXlpEisTWdfaXLPKPqq5DMDSgir4Llp6hKgbhntuPgMd4kDKxQ7izuip\nQ/whk02DXjjkXDCcxX+MpPLvsI8EPv2p5M4iTOrkZuepfAC1w2hwCCVZhc29GJhq\nL7hSo/wxAgMBAAECggEACNzyR/m0yGnorIsgwjNzhqNhCsncWauI56l4HzWGHnvj\nCos3fgWM6b24y9f2swigvjeDPWkmDtDMygmrG2SK1fF3tUeGeAIPbEF6g+qtIC6D\ny1ftDA6OvCX1xphbHakuqXsMj9zqzC2xXlTLHZrnsi8sD4yB2DG4FXzC0gqrT4eN\naF1ZRGgIRRTsOZLE9FfSECAfeGxMkvct44ka3CF8117GD4HKdk7c9zAleV9drTMS\nzlLm/TbA1PrtD6yhl9ZlLdGFNvtvEb+f9duoMHhJMgf9kkV7YSEt4FyCGBbxMmPn\njUkH4GAWAosItdIArhls8JnPLnF1Y9kJw74XN8ggwQKBgQDeyYsImx+N2ic5KGCU\nXH4h88Es17yd9kQI5Bll6My0Q5CbKFE4AX9zJz+h1jmPtJdmEdQc3dKrbqFS9hbo\nUWbzGFyEURJlgSitdDxzfo2kUuEhXnzpJ9XNc7o5vW/lvwzG6LuJyyk6LDm9A7dF\nM44dit1lh7uPnUwvMuilvEsQpwKBgQDDPNj9ngcprbUYG+YYqL9Iiw3EqKqfMF4d\n+sQuJ+/iIUdZyfNStn8XKr5EwojFaKVHelyVe5NX5xBPKRpppG3Zyk/+IpXaUPks\na0X7jDjbIzht0YZk6jYPb5pD1bgtkEvUBQLwkNogWQv5ddcEL/qOcAs3A6oBa8ww\nvuBpNiWPZwKBgQCw8xk346WjUHM45cYX8ILKSsXS7SNFv3T4LtuxYr88KjHv2ozv\n09i7yJ+01ebwKfraOizkJJeQTBVcRFObgBQp3t6UxaO79WMuqZTeEtVDgASKMXc6\n5MXfIhz2gW0f6KeoUO+rmS6MotXVN99X2oP1GPeSDcW+YqxBDMlV4gEpHwKBgHja\nmZek0tpB5O/flTd+fanyKbuqjLOtxRTZvca0ghGAeCJkmHKC9XjpxI3bvb41YNo2\nLEl8DWFEbxwRXYA3gKhoS8wMrKZPqW6NmOx1iFxeGPgh0wd007IKa3vCOBg8UjQU\nRhWUFPIhLQBmqYq2CGtTgB217u6iQ+uIvHFkSyvRAoGAR2m7TLznGq/m3wUMzFb0\n0FWJzvIEhrmlCd5tYBrZdhmhNxcj/Bx0ekqxK8nGoodZWUMm8IlKaTSp6sMz7TEk\ncL7SaCdEfG6+jX7tQcsB3/saRO2fqqD0aX0r6YmbyRMrT2ZRltR5aYIU8GRFxj+T\nHE/Mg8zG77leSiv/II8cCZM=\n-----END PRIVATE KEY-----\n",
         client_email:
